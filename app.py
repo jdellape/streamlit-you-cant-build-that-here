a591d2f4ff130833e84c29d2ac4527db77770708
John Dellape <dellapejw@gmail.com>
2023-11-17 20:17:12 -0500

    initial app

---
+++ app.py
@@ -0,0 +1,5 @@
+import streamlit as st
+import streamlit.components.v1 as components
+
+st.header("You Can't Build That Here")
+components.iframe("https://www.google.com/maps/d/embed?mid=1fVPHm7vdKd2rh57uCucrr_l3qS2GxoU&hl=en&ehbc=2E312F", width=640, height=480)
